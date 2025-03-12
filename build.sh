808e3e85e725f28a6351cf18c9c4235418e057b0
eduzayn <ti@eduzayn.com.br>
2025-03-12 04:30:56 +0000

    versel

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo "Current directory: $(pwd)"
-echo "Listing files: $(ls -la)"
-echo "Node version: $(node -v)"
-echo "NPM version: $(npm -v)"
-
-# Usar caminhos completos para os binários
-export PATH="$PATH:./node_modules/.bin"
-npx tsc --noEmitOnError false
-npx vite build
+echo "Running build script"
+npm run build
